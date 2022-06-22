faadd0f957ff542ab3c10cd46432224da3ed122c
Kaaay77 <96114448+Kaaay77@users.noreply.github.com>
2022-06-22 10:04:16 +0200

    full cambios

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+   }
 
-  add(item) {}
+  add(item) {
+    
+    this.items.push(item);
+    this.items.sort(function(a,b) {return a - b})
+    this.length += 1;
+    
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.pos.length === 0){
+      throw new Error('OutOfBounds');
+    }
+    for( let i = 0; i < this.length; i++){
+      if (this.length === pos){
+        return this.items[i];
+      }
+    }
+  }
 
-  max() {}
+  max() {
+    let maxim = 0;
+    for(let i = 0; i < this.elements.length; i++){
+      if (this.elements[i] > maxim){
+        maxim = this.elements[i]
+      }else{
+        continue
+      }
+      return maxim
+    }
+    
+  }
 
-  min() {}
+  min() {
+    if (this.pos.length === 0){
+      return 0;
+    }
+    let minim = 0;
+    for ( let i = 0; i < this.items.length; i++){
+      if (minim >= this.items[i]){
+        min = this.items[i];
+      }
+    }return minim
+  }
 
-  sum() {}
+  sum() {
+   let suma = 0;
+    for(let i = 0; i < this.length; i++){
+      suma += this.items[i]
+    }
+    return suma
+  }
 
-  avg() {}
+  avg() {
+    if (this.pos.length === 0){
+      return 0;
+    }
+   let average = this.items.reduce((a,b) => a + b)/ this.items.length;
+    return average
+  }
 }
 
 module.exports = SortedList;
